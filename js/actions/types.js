055d27a29ae76144a3c2ed2b3627c7dad495a59b
le-minhphuc <leminhphuc10t1@gmail.com>
2017-10-26 18:08:56 +0800

    Add framework for staff evaluation page

--- js/actions/types.js
+++ js/actions/types.js
@@ -4,7 +4,7 @@ export type Action =
   { type: 'USER_LOGIN', user: Object}
     | { type: 'USER_LOGOUT'}
     | { type: 'UPDATE_USER', user: any}
-    | { type: 'SWITCH_TAB', tab: 'home' | 'recent' | 'setting' }
+    | { type: 'SWITCH_TAB', tab: 'home' | 'recent' | 'setting' | 'evaluation' }
     | { type: 'UPDATE_TASKS', number: number}
 
 export type Dispatch = (action:Action | Array<Action>) => any;
