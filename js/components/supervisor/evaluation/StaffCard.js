82cf19634e1de9698aada873b1d3000e09db56e5
le-minhphuc <leminhphuc10t1@gmail.com>
2017-10-26 22:34:22 +0800

    Add modal box support for staff evaluation page

--- js/components/supervisor/evaluation/StaffCard.js
+++ js/components/supervisor/evaluation/StaffCard.js
@@ -3,7 +3,10 @@
 import React, { Component } from 'react';
 import { View, Text, Dimensions, TouchableHighlight } from 'react-native';
 import styles from './styles';
+<<<<<<< HEAD
 import { Image } from '../../common';
+=======
+>>>>>>> Add modal box support for staff evaluation page
 
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
@@ -17,6 +20,7 @@ export default class StaffCard extends Component {
     const staffName = this.props.name;
     return (
       <TouchableHighlight
+<<<<<<< HEAD
       style={{padding:5,height:70,width:deviceWidth}}
       activeOpacity={50}
       underlayColor={'darkgray'}
@@ -34,6 +38,19 @@ export default class StaffCard extends Component {
           <View style={{paddingLeft: 40, paddingBottom: 5, height:20, width:100, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
             <Text style={styles.nameLabel}>{staffName.substr(0, staffName.indexOf(' '))}</Text>
           </View>
+=======
+      style={{padding:5,height:70,width:deviceWidth,justifyContent:'center',alignItems:'flex-start',flexDirection:'row'}}
+      onPress={this.props.onClick}
+      >
+        <Image
+        circle
+        radius={30}
+        source={{uri:'https://s3.amazonaws.com/FringeBucket/default-user.png'}}
+        style={{borderWidth:1, borderColor:this.props.status === 'busy' ? 'red' : 'green'}}
+         />
+        <View style={{height:20, width:80, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={styles.nameLabel}>{staffName.substr(0, staffName.indexOf(' '))}</Text>
+>>>>>>> Add modal box support for staff evaluation page
         </View>
       </TouchableHighlight>
     );
--- js/components/supervisor/evaluation/styles.js
+++ js/components/supervisor/evaluation/styles.js
@@ -6,7 +6,11 @@ import { StyleSheet } from 'react-native';
 export default {
   nameLabel: {
     padding: 4,
+<<<<<<< HEAD
     width: 100,
     fontSize: 20,
+=======
+    width: 50
+>>>>>>> Add modal box support for staff evaluation page
   },
 };
