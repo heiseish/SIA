480e29abe02d4ed1068fbd24a282bc51ef53470c
heiseish <dtrnggiang@gmail.com>
2017-10-26 23:31:07 +0800

    fix conflict

--- js/components/supervisor/evaluation/styles.js
+++ js/components/supervisor/evaluation/styles.js
@@ -6,16 +6,10 @@ import { StyleSheet } from 'react-native';
 export default {
   nameLabel: {
     padding: 4,
-<<<<<<< HEAD
-<<<<<<< HEAD
     width: 100,
     fontSize: 20,
-=======
     width: 50
->>>>>>> Add modal box support for staff evaluation page
-=======
     width: 100,
     fontSize: 20,
->>>>>>> 17f394db709d0cc2de9fa24a1b39bf113c39eec5
   },
 };
